ce83a0aec5ded3d56bd9186774ed65a73012aeaf
Afloatbus <91826947+Afloatbus@users.noreply.github.com>
2023-09-21 18:33:25 +0100

    updated git-init.zsh

--- git-init.zsh
+++ git-init.zsh
@@ -7,7 +7,7 @@ gh repo create $1 --private
 echo "# $1" >> README.md
 git init
 git add README.md
-git commit -S -m "first commit"
+git commit -S -m "inital commit"
 git branch -M 
 git remote add origin git@github.com:Afloatbus/$1
 git push -u origin main
