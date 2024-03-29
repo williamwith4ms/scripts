cf636d82f1020517c8950e69be8798bba16cffd5
williammmm <91826947+williamwith4ms@users.noreply.github.com>
2024-03-29 18:15:52 +0000

    changed from zsh to bash

---
+++ git-init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Script will terminate."
+    exit 1
+fi
+gh repo create $1 --private
+echo "# $1" >> README.md
+git init
+git add README.md
+git commit -S -m "inital commit"
+git branch -M 
+git remote add origin git@github.com:$2/$1
+git push -u origin main
---
+++ git-push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Script will terminate."
+    exit 1
+fi
+
+git add -A
+git commit -S -m "$1"
+git branch -M 
+git push -u origin main
